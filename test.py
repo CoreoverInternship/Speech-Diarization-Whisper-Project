6d62c6b576efb0f22c5d13f5b3291248eaf861d6
root <root@bhrathgpt-v1.asia-south1-a.c.co-bharatgpt-prod.internal>
2024-05-27 15:42:54 +0000

    making file that gets olny corrected dataset

--- test.py
+++ test.py
@@ -1,3 +1,24 @@
-import torch
+import pandas as pd
 
-print(torch.cuda.is_available())
+import os
+
+directory_path = '/root/Speech_diarisation/NewData/NewData/Custom_Training_Set'
+
+# Iterating through the directory
+for filename in os.listdir(directory_path):
+    file_path = os.path.join(directory_path, filename)
+    
+    if (os.path.isdir(file_path) and filename[:9] == "corrected" ):
+        print("Directory:"+ file_path )
+        
+# Data to be written to CSV
+# data = {
+#     "file": ["Alice", "Bob", "Charlie"],
+#     "transcript": [30, 25, 35],  
+# }
+
+# # Creating a DataFrame
+# df = pd.DataFrame(data)
+
+# # Writing to CSV
+# df.to_csv('training.csv', index=False)
